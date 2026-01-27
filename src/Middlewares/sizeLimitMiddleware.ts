fdde8803c4e6a0b12c0a2ee2f690d91214382fa2
Gvinfinity <gabrielsoares@berry.dev.br>
2026-01-27 18:37:28 -0300

    fix: Corrige o último PR adicionando middleware para JSON
    Eu sou burro foi mal, JSON não usa send

--- src/Middlewares/sizeLimitMiddleware.ts
+++ src/Middlewares/sizeLimitMiddleware.ts
@@ -2,6 +2,30 @@ import type { NextFunction, Request, Response } from "express";
 
 function sizeLimitMiddleware(_req: Request, res: Response, next: NextFunction) {
     const oldSend = res.send;
+    const oldJson = res.json;
+
+    res.json = function (body?: unknown): Response {
+        const sizeLimit = 1024 * 1024 * 31; // 31 MB limit
+        const bodySize = Buffer.byteLength(
+            typeof body === "string" ? body : JSON.stringify(body)
+        );
+
+        if (bodySize > sizeLimit) {
+            res.status(418);
+            return oldSend.call(
+                res,
+                JSON.stringify({
+                    error: "Response Object Too Large",
+                    message: `Response size of ${(
+                        bodySize /
+                        (1024 * 1024)
+                    ).toFixed(2)} MB exceeds the limit of 31 MB.`
+                })
+            );
+        }
+
+        return oldJson.call(res, body);
+    };
 
     res.send = function (body?: unknown): Response {
         const sizeLimit = 1024 * 1024 * 31; // 31 MB limit
